40099214ebb90781920e2c9d59ad0c6f0592d072
ShinDarth <borzifrancesco@gmail.com>
2014-12-21 16:38:46 +0100

    getCoord()

--- js/othello.js
+++ js/othello.js
@@ -1,6 +1,11 @@
 (function (){
   var app = angular.module('othello', [ ]);
 
+  app.controller('othelloCtrl', function() {
+    this.prova = "ciao ciao";
 
+    this.getCoords = function(x, y) {
+      alert(x + ", " + y);
+    };
   });
 })();
