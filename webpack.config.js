ae08a0b64a2cb0efe2028b195935595cf3da0f79
ShinDarth <borzifrancesco@gmail.com>
2017-02-28 15:26:39 +0100

    code cleaning (fix indentation & css warnings)

--- webpack.config.js
+++ webpack.config.js
@@ -1,11 +1,11 @@
 var path = require('path');
 
 module.exports = {
-  entry: {
-    app: './app/index.js'
-  },
-  output: {
-    filename: 'othello-bundle.js',
-    path: path.resolve(__dirname, 'app/dist')
-  }
+    entry: {
+        app: './app/index.js'
+    },
+    output: {
+        filename: 'othello-bundle.js',
+        path: path.resolve(__dirname, 'app/dist')
+    }
 };
